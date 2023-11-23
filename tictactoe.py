aaece4d5cb7c125c2f5494f9f7f59295ad556f1a
ankurt02 <ankurt5202@gmail.com>
2023-11-23 22:54:27 +0530

    updated tic_tac_toe

--- tictactoe.py
+++ tictactoe.py
@@ -1,62 +1,77 @@
-import os 
+import os
 
-turn = 'X'
-win = False
-spaces = 9
-
-def draw(board):
+def drawBoard(board):
     for i in range(6, -1, -3):
-        print(' ' + board[i] + '|' + board[i+1] + '|' + board[i+2])
-        
-def takeinput(board, spaces, turn):
-    pos =-1
-    print("{}'s turn : ".format(turn))
+        print(' ' + board[i] + '|' +
+              board[i+1] + '|' + board[i+2])
+
+def playerInput(board, turn):
+    pos = -1
+    print(turn + "'s turn:")
     while pos == -1:
         try:
-            print("Pick position 1-9 : ")
-            pos=int(input())
-            if (pos<1 or pos>9):
-                pos=-1
-            elif board[pos-1] != ' ':
-                pos=-1
-        except:
-            print("Enter a valid positon")
-    spaces -=1
-    board[pos-1] =turn
-    if turn=='X':
-        turn='0'
-    else:
-        turn='X'
-    return board, spaces, turn
+            print("Pick position 1-9:")
+            pos = int(input())
+            if not (1 <= pos <= 9) or board[pos - 1] != ' ':
+                print("Invalid position. Try again.")
+                pos = -1
+        except ValueError:
+            print("Enter a valid number.")
 
-def checkwin(board):
-    for i in range(3):
-        r=i*3
-        if board[r] != ' ':
-            if board[r] == board[r+1] and board[r+1] ==board[r+2]:
-                return board[r]
-    if board[i] != ' ':
-        if board[i] == board[i+3] and board[i] ==board[i+6]:
+    board[pos - 1] = turn
+    return board
+
+def checkWinner(board):
+    for i in range(0, 3):
+        r = i * 3
+        if board[r] != ' ' and board[r] == board[r+1] == board[r+2]:
+            return board[r]
+
+    for i in range(0, 3):
+        if board[i] != ' ' and board[i] == board[i+3] == board[i+6]:
             return board[i]
-    if board[0] != ' ':
-        if (board[0] == board[4] and board[4] == board[8]) :
-            return board[0]
-    if board[2] != ' ':
-        if (board[2] == board[4] and board[4] == board[6]):
-            return board[2]
+
+    if board[0] != ' ' and board[0] == board[4] == board[8]:
+        return board[0]
+
+    if board[2] != ' ' and board[2] == board[4] == board[6]:
+        return board[2]
+
     return 0
 
+def is_board_full(board):
+    return ' ' not in board
 
-board = [' '] * 9
-while not win and spaces:
-    draw(board)
-    board, spaces, turn = takeinput(board, spaces, turn)
-    win = checkwin(board)
-    os.system('cls')
+def resultOfGame(result):
+    if result == 0:
+        print("It's a draw!")
+    else:
+        print('{} wins!'.format(result))
+
+def clearScreen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def main():
+    turn = 'X'
+    board = [' '] * 9
+    # player1=input("Enter player 1 name : ")
+    # player2= input("Enter player 2 name :")
+
+    while True:
+        clearScreen()
+        drawBoard(board)
+        board = playerInput(board, turn)
+        result = checkWinner(board)
+
+        if result or is_board_full(board):
+            clearScreen()
+            drawBoard(board)
+            resultOfGame(result)
+            break
+
+        turn = 'O' if turn == 'X' else 'X'
+
+if __name__ == "__main__":
+    main()
     
-draw(board)
-if not win and not spaces:
-    print('Draw')
-else:
-    print("{} wins".format(win))
-    input()
+    
